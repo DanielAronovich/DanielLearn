a7b273038179b04c3552fbdb6592f4c39c55fd22
Daniel <aronovich.daniel@gmail.com>
2021-06-14 15:47:49 +0300

    Added logsitic regression

--- main.py
+++ main.py
@@ -1,6 +1,77 @@
+import pandas as pd
+import numpy as np
+from numpy import log, dot, e
+from numpy.random import rand
+import matplotlib.pyplot as plt
+from sklearn.metrics import confusion_matrix, classification_report
+from sklearn.datasets import load_breast_cancer
+from sklearn.preprocessing import MinMaxScaler, StandardScaler
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import roc_auc_score
+
+class LogisticRegression:
+
+    def sigmoid(self, z): return 1 / (1 + e ** (-z))
+
+    def cost_function(self, X, y, weights):
+        z = dot(X, weights)
+        predict_1 = y * log(self.sigmoid(z))
+        predict_0 = (1 - y) * log(1 - self.sigmoid(z))
+        return -sum(predict_1 + predict_0) / len(X)
+
+    def fit(self, X, y, epochs=25, lr=0.05):
+        loss = []
+        weights = rand(X.shape[1])
+        N = len(X)
+
+        for _ in range(epochs):
+            # Gradient Descent
+            y_hat = self.sigmoid(dot(X, weights))
+            weights -= lr * dot(X.T, y_hat - y) / N
+            # Saving Progress
+            loss.append(self.cost_function(X, y, weights))
+
+        self.weights = weights
+        self.loss = loss
+
+    def predict(self, X):
+        # Predicting with sigmoid function
+        z = dot(X, self.weights)
+        # Returning binary result
+        return [1 if i > 0.5 else 0 for i in self.sigmoid(z)]
 
 
 if __name__ == "__main__":
 
     print('Humus!')
 
+X = load_breast_cancer()['data']
+y = load_breast_cancer()['target']
+feature_names = load_breast_cancer()['feature_names']
+
+
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.33, random_state=42)
+
+
+scaler = StandardScaler()
+X_train = scaler.fit_transform(X_train)
+X_test = scaler.transform(X_test)
+
+logreg = LogisticRegression()
+logreg.fit(X_train, y_train, epochs=500, lr=0.5)
+y_pred = logreg.predict(X_test)
+
+
+print("ROC AUC",roc_auc_score(y_test, y_pred))
+print(classification_report(y_test, y_pred))
+
+
+plt.style.use('seaborn-whitegrid')
+plt.rcParams['figure.dpi'] = 227
+plt.rcParams['figure.figsize'] = (16,5)
+plt.plot(logreg.loss)
+plt.title('Logistic Regression Training', fontSize=15)
+plt.xlabel('Epochs', fontSize=12)
+plt.ylabel('Loss', fontSize=12)
+plt.show()
